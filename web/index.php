e67606bffc47aba4fe0475eebde5a37b50fb6ce4
Konstantinos Christofilos <k.christofilos@intelen.com>
2015-01-26 00:26:27 +0200

    Add twig engine for templating

--- web/index.php
+++ web/index.php
@@ -5,7 +5,15 @@ $config = require('../config/settings.inc.php');
 
 use Rocket\Models\Kernel;
 
-$kernel = new Kernel($config);
+$loader = new Twig_Loader_Filesystem('../templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+));
+// $twig = new Twig_Environment($loader, array(
+//     'cache' => '../cache',
+// ));
+
+$kernel = new Kernel($config, $twig);
 $response = $kernel->parseRequest();
 
 echo $response;
