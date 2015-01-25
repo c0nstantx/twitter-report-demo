02dbd73c9da12d5aeb8dfb983c92c0dd62f5f55e
Konstantinos Christofilos <k.christofilos@intelen.com>
2015-01-25 22:55:44 +0200

    Clear some old settings data

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -4,12 +4,3 @@ define('CONSUMER_KEY', 'DVXHssj5CskPZaSlTPBpqpkzY');
 define('CONSUMER_SECRET', 'CaEIf10GgyGQc1U7IsCVWtQA3t152PAHD1S99zMqzleUyIYvkw');
 define('TWEET_DEMO_REPORT_URL', 'https://rocket-tweet-demo.herokuapp.com/');
 define('DEFAULT_TWEET_LIMIT', 300);
-
-return array(
-    'consumer_key'    => 'DVXHssj5CskPZaSlTPBpqpkzY',
-    'consumer_secret' => 'CaEIf10GgyGQc1U7IsCVWtQA3t152PAHD1S99zMqzleUyIYvkw',
-    'token'           => '265929407-dkT7Hm8R7NAdDHFKcKbW7ZgBjTVWYA621pg2UW7H',
-    'secret'          => 'zJyXL7il0u8MxABBCc3qMaupXIwKsx72oUj0a1VhDemLc',
-    // 'bearer'          => 'YOUR_OAUTH2_TOKEN',
-    'user_agent'      => 'RocketGraph Tweet Demo Report',
-    );
