2136c9c1f41ef1f6278754ccb51e9840d9bcf38b
Konstantinos Christofilos <k.christofilos@intelen.com>
2015-01-26 21:59:34 +0200

    Disable warnings for production

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
 
 define('CONSUMER_KEY', 'DVXHssj5CskPZaSlTPBpqpkzY');
 define('CONSUMER_SECRET', 'CaEIf10GgyGQc1U7IsCVWtQA3t152PAHD1S99zMqzleUyIYvkw');
